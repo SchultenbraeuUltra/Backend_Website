9a36bf7c4a3f8e0f2752f439a7828d69b21b988f
Isabel Monreal <isa.novu@gmail.com>
2020-04-18 19:38:50 +0200

    Frontend url angepasst

--- backend/versuch/public/javascripts/frontend.js
+++ backend/versuch/public/javascripts/frontend.js
@@ -13,7 +13,7 @@ setInterval(load, 3000);
 function load() {
 	check(); 
 	if(CHANGE) {
-		const url = 'ai-info.informatik.hs-fulda.de:443';
+		const url = 'ai-info.informatik.hs-fulda.de:443/users/adresse';
 		Http.open("GET", url);
 		Http.send();
 	}
@@ -30,7 +30,7 @@ Http.onreadystatechange = function(){
 }
 
 function check() {
-	const url = 'ai-info.informatik.hs-fulda.de:443';
+	const url = 'ai-info.informatik.hs-fulda.de:443/users/change';
 	HttpChanged.open("GET", url);
 	HttpChanged.send();
 }
